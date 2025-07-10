41f8c50a452b6bb21c25f9dc50f292a9cad2a035
bnishantd <dwivedinishant.n@gmail.com>
2025-07-11 01:04:51 +0530

    added product icon

--- src/main/main.js
+++ src/main/main.js
@@ -1,9 +1,14 @@
 import { app, BrowserWindow } from "electron/main";
+import path from "node:path";
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    // width: 550,
-    // height: 300,
+    width: 800,
+    height: 600,
+    icon: path.join("../../assets/veya-logo.png"), // Use .ico for Windows
+    webPreferences: {
+      preload: path.join('../preload.js')
+    }
   });
   win.loadFile("public/index.html");
 };
